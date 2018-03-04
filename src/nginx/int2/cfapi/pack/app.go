1cf651b52b6827dc5d1e3028cd6195400b79e385
Dave Goddard <dave@goddard.id.au>
2018-03-04 10:45:35 -0400

    cfapi.pack almost works on int2

--- src/nginx/int2/cfapi/pack/app.go
+++ src/nginx/int2/cfapi/pack/app.go
@@ -3,16 +3,23 @@ package pack
 import (
 	"bytes"
 	"context"
+	"crypto/md5"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/cloudfoundry/libbuildpack"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
@@ -34,6 +41,22 @@ func (a *App) Buildpacks(buildpacks []string) {
 
 func (a *App) ConfirmBuildpack(version string) error {
 	return nil
+}
+
+func (a *App) setupBuildpackDir(buildpacks []string) error {
+	configs := []string{}
+	for _, buildpackName := range buildpacks {
+		buildpackDir := filepath.Join(a.tmpPath, "buildpacks", fmt.Sprintf("%x", md5.Sum([]byte(buildpackName))))
+		if err := os.MkdirAll(buildpackDir, 0755); err != nil {
+			return err
+		}
+		if err := libbuildpack.ExtractZip(a.cluster.buildpack(buildpackName), buildpackDir); err != nil {
+			return err
+		}
+		configs = append(configs, fmt.Sprintf(`{"name":"%s","uri":""}`, buildpackName))
+	}
+	configJson := "[" + strings.Join(configs, ",") + "]"
+	return ioutil.WriteFile(filepath.Join(a.tmpPath, "buildpacks", "config.json"), []byte(configJson), 0644)
 }
 
 func (a *App) Stage() error {
@@ -71,15 +94,14 @@ func (a *App) Stage() error {
 		return err
 	}
 	// TODO Get correct dirname and handle set buildpacks
-	if err := os.MkdirAll(filepath.Join(a.tmpPath, "buildpacks", "acf92879c5d3998e550daedb7ab6b513"), 0755); err != nil {
-		return err
-	}
-	if err := libbuildpack.ExtractZip(a.cluster.buildpack(a.cluster.defaultBuildpackName), filepath.Join(a.tmpPath, "buildpacks", "acf92879c5d3998e550daedb7ab6b513")); err != nil {
-		return err
-	}
-	configJson := `[{"name":"nginx-buildpack","uri":""}]`
-	if err := ioutil.WriteFile(filepath.Join(a.tmpPath, "buildpacks", "config.json"), []byte(configJson), 0644); err != nil {
-		return err
+	if len(a.buildpacks) > 0 {
+		if err := a.setupBuildpackDir(a.buildpacks); err != nil {
+			return err
+		}
+	} else {
+		if err := a.setupBuildpackDir([]string{a.cluster.defaultBuildpackName}); err != nil {
+			return err
+		}
 	}
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
@@ -134,7 +156,7 @@ func (a *App) Stage() error {
 
 	out2, err := cli.ContainerLogs(ctx, containerID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
 	if err != nil {
-		panic(err)
+		return err
 	}
 	io.Copy(&a.Stdout, out2)
 
@@ -163,7 +185,81 @@ func (a *App) Stage() error {
 }
 
 func (a *App) Run() error {
-	return nil
+	// docker run --rm -P -v "$(pwd)/out:/workspace" packs/cf:run
+	fmt.Println("**** Running pack.App.Run: ", a.tmpPath)
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	imageName := "packs/cf:run"
+
+	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+	io.Copy(&a.Stdout, out)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:        imageName,
+		AttachStdout: true,
+		AttachStderr: true,
+		Tty:          true,
+	}, &container.HostConfig{
+		AutoRemove:      true,
+		PublishAllPorts: true,
+		Binds: []string{
+			filepath.Join(a.tmpPath, "out") + ":/workspace",
+		},
+	}, nil, "")
+	if err != nil {
+		return err
+	}
+	containerID := resp.ID
+
+	if err := cli.ContainerStart(ctx, containerID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	fmt.Println("**** ContainerID:", containerID)
+
+	filter := filters.NewArgs()
+	filter.Add("id", containerID)
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{Filters: filter})
+	if err != nil {
+		return err
+	}
+	if len(containers) < 1 {
+		return fmt.Errorf("Could not find container with ID: %s", containerID)
+	} else if len(containers) > 1 {
+		return fmt.Errorf("Found %d containers with ID: %s", len(containers), containerID)
+	}
+	a.port = fmt.Sprintf("%d", containers[0].Ports[0].PublicPort)
+	fmt.Println("**** Port:", a.port)
+
+	out2, err := cli.ContainerLogs(ctx, containerID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
+	if err != nil {
+		return err
+	}
+	go io.Copy(&a.Stdout, out2)
+
+	// TODO duplicate from cflocal
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%s", a.port))
+			if err == nil {
+				conn.Close()
+				return nil
+			}
+		case <-time.After(3 * time.Second):
+			return fmt.Errorf("Timed out waiting to connect to port %s", a.port)
+		}
+	}
 }
 
 func (a *App) Push() error {
@@ -183,12 +279,67 @@ func (a *App) PushAndConfirm() error {
 	// Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
 	return nil
 }
-func (a *App) Destroy() error {
+func (a *App) Stop() error {
+	// TODO need to store containerID for RUN()
+	// if err := cli.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{Force: true}); err != nil {
+	// 	fmt.Println("Failed to remove container")
+	// }
 	return nil
 }
-func (a *App) GetBody(path string) (string, error) {
-	return "", nil
+func (a *App) Destroy() error {
+	a.Stop()
+	return os.RemoveAll(a.tmpPath)
 }
+
+func (a *App) GetUrl(path string) (string, error) {
+	if a.port == "" {
+		return "", fmt.Errorf("app does not have a known running port")
+	}
+	return fmt.Sprintf("http://localhost:%s%s", a.port, path), nil
+}
+func (a *App) Get(path string, headers map[string]string) (string, map[string][]string, error) {
+	url, err := a.GetUrl(path)
+	if err != nil {
+		return "", map[string][]string{}, err
+	}
+	client := &http.Client{}
+	if headers["NoFollow"] == "true" {
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+		delete(headers, "NoFollow")
+	}
+	req, _ := http.NewRequest("GET", url, nil)
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	if headers["user"] != "" && headers["password"] != "" {
+		req.SetBasicAuth(headers["user"], headers["password"])
+		delete(headers, "user")
+		delete(headers, "password")
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", map[string][]string{}, err
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", map[string][]string{}, err
+	}
+	resp.Header["StatusCode"] = []string{strconv.Itoa(resp.StatusCode)}
+	return string(data), resp.Header, err
+}
+
+func (a *App) GetBody(path string) (string, error) {
+	body, _, err := a.Get(path, map[string]string{})
+	// TODO: Non 200 ??
+	// if !(len(headers["StatusCode"]) == 1 && headers["StatusCode"][0] == "200") {
+	// 	return "", fmt.Errorf("non 200 status: %v", headers)
+	// }
+	return body, err
+}
+
 func (a *App) Log() string {
 	return a.Stdout.String()
 }
